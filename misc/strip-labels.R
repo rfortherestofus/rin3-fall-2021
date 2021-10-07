bb0b218346d858ef58758c6df429efbe6f268dfd
charliejhadley <charliejhadley@gmail.com>
2021-10-07 12:51:19 +0100

    Update strip-labels.R

--- misc/strip-labels.R
+++ misc/strip-labels.R
@@ -1,6 +1,12 @@
 library(tidyverse)
 library(haven)
 
+# Data file from https://www.thearda.com/Archive/Files/Downloads/GSS1991_DL2.asp
+# 
+# https://rstudio-pubs-static.s3.amazonaws.com/466787_84982191b86e4591bfcee776e498a6d4.html
+# 
+# https://www.pipinghotdata.com/posts/2020-12-23-leveraging-labelled-data-in-r/
+
 download.file("https://osf.io/2qphn/download",
               destfile = "data/General Social Survey_1991.SAV")
 

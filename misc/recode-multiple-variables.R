8f0807db9308922373fe14dea9f65fc02b36306f
David Keyes <david@rfortherestofus.com>
2021-11-07 07:08:02 -0800

    Add v2 of recode_to_y_n

--- misc/recode-multiple-variables.R
+++ misc/recode-multiple-variables.R
@@ -15,6 +15,10 @@ household_data %>%
     radio == 1 ~ "Y"
   ))
 
+
+
+# Attempt #1 --------------------------------------------------------------
+
 recode_to_y_n <- function(data, variable_name) {
   
   data %>% 
@@ -24,3 +28,22 @@ recode_to_y_n <- function(data, variable_name) {
 
 household_data %>% 
   recode_to_y_n(radio)
+
+
+# Attempt #2 (Vector) -----------------------------------------------------
+
+household_data$radio
+
+if_else(household_data$radio == "1", "Y", "N")
+
+recode_to_y_n_v2 <- function(vector) {
+  if_else(vector == "1", "Y", "N") %>% as_factor()
+}
+
+recode_to_y_n_v2(household_data$radio)
+
+household_data %>% 
+  mutate(radio = recode_to_y_n_v2(radio))
+
+household_data %>% 
+  mutate(across(c(ever_married, radio), recode_to_y_n_v2))
